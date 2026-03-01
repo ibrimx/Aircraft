85d31032a5b87a1d1093d6f828137accda12bbfe
Ibrim <159613319+ibrimx@users.noreply.github.com>
2026-03-01 22:05:07 +0300

    feat: add Next.js root page with edge runtime

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
-import { AppShell } from '../src/shell/app-shell';
+export const runtime = 'edge'
 
-export const runtime = 'edge';
+import { AppShell } from '../src/shell/app-shell'
 
 export default function RootPage() {
-  return <AppShell />;
+  return <AppShell />
 }
